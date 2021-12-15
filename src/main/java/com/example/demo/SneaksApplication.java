59c82559066b4b0b3aa312b313b3bbee1bc1f4ca
windcalm666 <79242740+windcalm666@users.noreply.github.com>
2021-12-15 12:52:17 +0900

    環境構築

---
+++ src/main/java/com/example/demo/SneaksApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SneaksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SneaksApplication.class, args);
+	}
+
+}
